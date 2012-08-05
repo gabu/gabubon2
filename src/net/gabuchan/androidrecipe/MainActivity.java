70940110dafaefa824b29bbb100f1f147846d4a3
gabu <tsukada.shouya@gmail.com>
2012-08-06 01:48:51 +0900

    066できたー

--- src/net/gabuchan/androidrecipe/MainActivity.java
+++ src/net/gabuchan/androidrecipe/MainActivity.java
@@ -53,6 +53,7 @@ import net.gabuchan.androidrecipe.recipe062.Recipe062Activity;
 import net.gabuchan.androidrecipe.recipe063.Recipe063Activity;
 import net.gabuchan.androidrecipe.recipe064.Recipe064Activity;
 import net.gabuchan.androidrecipe.recipe065.Recipe065Activity;
+import net.gabuchan.androidrecipe.recipe066.Recipe066Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -116,6 +117,7 @@ public class MainActivity extends ListActivity {
         mRecipes.add(new Recipe(R.string.recipe_063_title, Recipe063Activity.class));
         mRecipes.add(new Recipe(R.string.recipe_064_title, Recipe064Activity.class));
         mRecipes.add(new Recipe(R.string.recipe_065_title, Recipe065Activity.class));
+        mRecipes.add(new Recipe(R.string.recipe_066_title, Recipe066Activity.class));
 
         List<String> titles = new ArrayList<String>();
         for (Recipe recipe : mRecipes) {
---
+++ src/net/gabuchan/androidrecipe/recipe066/Recipe066Activity.java
@@ -0,0 +1,51 @@
+
+package net.gabuchan.androidrecipe.recipe066;
+
+import net.gabuchan.androidrecipe.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Recipe066Activity extends Activity {
+    private static final int REQUEST_CODE = 1;
+
+    ImageView mImageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_066);
+        mImageView = (ImageView) findViewById(R.id.image_view);
+    }
+
+    public void onButtonClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            // 選択された画像のUriを取得
+            Uri uri = data.getData();
+            // Uriからidだけ抜き出す
+            String uriString = uri.toString();
+            System.out.println(uriString);
+            long id = Long.parseLong(uriString.substring(uriString.lastIndexOf("/") + 1));
+            // サムネイル画像を取得
+            Bitmap thumbnail = MediaStore.Images.Thumbnails.getThumbnail(
+                    getContentResolver(),
+                    id,
+                    MediaStore.Images.Thumbnails.MINI_KIND,
+                    null);
+            // ImageViewにセット
+            mImageView.setImageBitmap(thumbnail);
+        }
+    }
+}
