073e153d3e9f01297f01a0fe8c361e1689dea2ef
gabu <tsukada.shouya@gmail.com>
2012-08-05 17:39:28 +0900

    058できたー

--- src/net/gabuchan/androidrecipe/MainActivity.java
+++ src/net/gabuchan/androidrecipe/MainActivity.java
@@ -45,6 +45,7 @@ import net.gabuchan.androidrecipe.recipe053.Recipe053Activity;
 import net.gabuchan.androidrecipe.recipe054.Recipe054Activity;
 import net.gabuchan.androidrecipe.recipe056.Recipe056Activity;
 import net.gabuchan.androidrecipe.recipe057.Recipe057Activity;
+import net.gabuchan.androidrecipe.recipe058.Recipe058Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -100,6 +101,7 @@ public class MainActivity extends ListActivity {
         mRecipes.add(new Recipe(R.string.recipe_054_title, Recipe054Activity.class));
         mRecipes.add(new Recipe(R.string.recipe_056_title, Recipe056Activity.class));
         mRecipes.add(new Recipe(R.string.recipe_057_title, Recipe057Activity.class));
+        mRecipes.add(new Recipe(R.string.recipe_058_title, Recipe058Activity.class));
 
         List<String> titles = new ArrayList<String>();
         for (Recipe recipe : mRecipes) {
---
+++ src/net/gabuchan/androidrecipe/recipe058/Recipe058Activity.java
@@ -0,0 +1,14 @@
+
+package net.gabuchan.androidrecipe.recipe058;
+
+import net.gabuchan.androidrecipe.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Recipe058Activity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_058);
+    }
+}
