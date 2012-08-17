b702af583262198c402707b98aefec1ad21c98e8
LuckOfWise <luck.of.wise@gmail.com>
2012-08-18 01:59:03 +0900

    114で文字数カウントしてボタンの制御するようにしちゃった☆

--- src/net/gabuchan/androidrecipe/recipe114/Recipe114Activity.java
+++ src/net/gabuchan/androidrecipe/recipe114/Recipe114Activity.java
@@ -18,13 +18,18 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Recipe114Activity extends Activity {
@@ -43,6 +48,8 @@ public class Recipe114Activity extends Activity {
     private RequestToken mRequestToken;
     private ProgressDialog mProgressDialog;
     private EditText mEditText;
+    private TextView mTextView;
+    private Button mButton;
 
     private Recipe114Handler mHandler;
 
@@ -96,6 +103,39 @@ public class Recipe114Activity extends Activity {
         setContentView(R.layout.activity_recipe_114);
 
         mEditText = (EditText) findViewById(R.id.status);
+        mTextView = (TextView) findViewById(R.id.count);
+        mButton = (Button) findViewById(R.id.tweet);
+        // 文字数をカウントしてボタンを制御する
+        mEditText.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				int textColor;
+				int length = 140 - s.length();
+				// 140文字をオーバーした時は文字数を赤色に
+				if (length < 0) {
+					textColor = Color.RED;
+				}else {
+					textColor = Color.BLACK;
+				}
+				mTextView.setTextColor(textColor);
+				mTextView.setText(String.valueOf(length));
+				// 文字数が0文字または140文字以上の時はボタンを無効
+				if (s.length() == 0 || s.length() > 140) {
+					mButton.setEnabled(false);
+				} else {
+					mButton.setEnabled(true);
+				}
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
+		});
+        
         mHandler = new Recipe114Handler(this);
 
         if (hasAccessToken()) {
