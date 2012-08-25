dde1e1c27411664a8e20a58d87ac769434819ba4
gabu <tsukada.shouya@gmail.com>
2012-08-25 14:21:39 +0900

    fix #22 位置情報の設定がOFFになっている？トーストを表示する

--- src/net/gabuchan/androidrecipe/recipe088/Recipe088Activity.java
+++ src/net/gabuchan/androidrecipe/recipe088/Recipe088Activity.java
@@ -9,6 +9,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Recipe088Activity extends Activity {
     private LocationManager mLocationManager;
@@ -19,14 +20,17 @@ public class Recipe088Activity extends Activity {
         public void onStatusChanged(String provider, int status, Bundle extras) {
             // プロバイダの状態が変化したら呼び出される。
         }
+
         @Override
         public void onProviderEnabled(String provider) {
             // プロバイダが有効になったら呼び出される。
         }
+
         @Override
         public void onProviderDisabled(String provider) {
             // プロバイダが無効になったら呼び出される。
         }
+
         @Override
         public void onLocationChanged(Location location) {
             // 位置情報が取得できるたびに呼び出される。
@@ -56,7 +60,9 @@ public class Recipe088Activity extends Activity {
         String provider = mLocationManager.getBestProvider(new Criteria(), true);
         if (provider == null) {
             // 位置情報が取得できるプロバイダがありません。
-            // Wifiにも3Gにも繋がっていないなど。
+            // 位置情報の設定がOFFになっているなど。
+            Toast.makeText(this, "位置情報の設定がOFFになっていると位置情報は取得できません",
+                    Toast.LENGTH_SHORT).show();
         } else {
             // リスナーをセットして位置情報の取得を開始
             mLocationManager.requestLocationUpdates(provider, 0, 0, mListener);
