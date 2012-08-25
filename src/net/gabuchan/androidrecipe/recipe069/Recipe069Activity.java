17cf15c9b678dba87949cdac135063434df8c5bd
gabu <tsukada.shouya@gmail.com>
2012-08-25 15:03:04 +0900

    ロックスクリーンで再開しないようにonResume()ではなくonWindowFocusChanged()で再開するように変更

--- src/net/gabuchan/androidrecipe/recipe069/Recipe069Activity.java
+++ src/net/gabuchan/androidrecipe/recipe069/Recipe069Activity.java
@@ -4,7 +4,6 @@ package net.gabuchan.androidrecipe.recipe069;
 import net.gabuchan.androidrecipe.R;
 import android.app.Activity;
 import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
 
 public class Recipe069Activity extends Activity {
@@ -15,29 +14,41 @@ public class Recipe069Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_069);
+
+        // MediaPlayerを生成
+        mMediaPlayer = MediaPlayer.create(this, R.raw.sound);
+        mMediaPlayer.setLooping(true);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        // MediaPlayerを生成
-        mMediaPlayer = MediaPlayer.create(this, R.raw.sound);
-        mMediaPlayer.setLooping(true);
-        mMediaPlayer.setOnPreparedListener(new OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.start();
-            }
-        });
+        // onResume()で再生しちゃうと
+        // ロックスクリーンの時点で再生されてしまうので
+        // onWindowFocusChanged()で再生するよ
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // 音声再生をスタート
+            mMediaPlayer.start();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        // 再生中なら停止
+        // 再生中なら一時停止
         if (mMediaPlayer.isPlaying()) {
-            mMediaPlayer.stop();
+            mMediaPlayer.pause();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
         // 解放
         mMediaPlayer.release();
     }
